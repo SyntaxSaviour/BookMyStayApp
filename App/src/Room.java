c7d2abcc001679a4300568eb5ddfd3601fe1529b
SyntaxSaviour <farjanalam16@gmail.com>
2026-03-10 15:00:32 +0530

    Farjan Alam Added UC3-BasicRoomTypeAndStaticAvailability

---
+++ App/src/Room.java
@@ -0,0 +1,36 @@
+abstract class Room {
+    private String type;
+    private int beds;
+    private int size;
+    private double price;
+
+    public Room(String type, int beds, int size, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getBeds() {
+        return beds;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Room Type: " + type);
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sq ft");
+        System.out.println("Price: $" + price);
+    }
+}
