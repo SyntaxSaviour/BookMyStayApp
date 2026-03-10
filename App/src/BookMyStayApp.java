b0ec8e67eec6b03545493525fdee56860863de72
SyntaxSaviour <farjanalam16@gmail.com>
2026-03-10 14:51:26 +0530

    Farjan Alam Added UC1-WelcomePage

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,11 @@
+public class BookMyStayApp {
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println(" Welcome to the Book My Stay App ");
+        System.out.println(" Version : 1.0 ");
+        System.out.println("======================================");
+
+        System.out.println("System initialized successfully.");
+    }
+}
